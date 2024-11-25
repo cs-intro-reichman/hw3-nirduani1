174dd9879d19f2f916a19ca85ab68d353f1f43d2
nirduani1 <nirduani@icloud.com>
2024-11-25 15:09:29 +0200

    done

--- LoanCalc.java
+++ LoanCalc.java
@@ -28,8 +28,11 @@ public class LoanCalc {
 	// Computes the ending balance of a loan, given the loan amount, the periodical
 	// interest rate (as a percentage), the number of periods (n), and the periodical payment.
 	private static double endBalance(double loan, double rate, int n, double payment) {	
-		// Replace the following statement with your code
-		return 0;
+		double balance = loan ;
+		   for(int i = 0 ; i < n ; i++){
+		       balance = (balance-payment)*(1+rate/100) ;
+		    }
+		return balance ;
 	}
 	
 	// Uses sequential search to compute an approximation of the periodical payment
@@ -38,8 +41,14 @@ public class LoanCalc {
 	// the number of periods (n), and epsilon, the approximation's accuracy
 	// Side effect: modifies the class variable iterationCounter.
     public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {
-		// Replace the following statement with your code
-		return 0;
+		 double g = loan/n ;
+		 double f = endBalance(loan, rate, n, g) ;
+		    while (Math.abs(f) > epsilon){
+				g = g + epsilon;
+				f = endBalance(loan, rate, n, g);
+				iterationCounter ++ ;
+			}
+		return g;
     }
     
     // Uses bisection search to compute an approximation of the periodical payment 
